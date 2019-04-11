f4465645bec63adb3174c3c9d0df99d28c25b90f
davenkin <davenkin@126.com>
2019-04-11 17:18:40 +0800

    added cors support

---
+++ src/main/java/com/ecommerce/order/common/configuration/CorsConfiguration.java
@@ -0,0 +1,19 @@
+package com.ecommerce.order.common.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
