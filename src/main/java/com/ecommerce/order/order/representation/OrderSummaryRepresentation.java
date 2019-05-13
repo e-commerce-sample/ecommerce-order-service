300eb0b2bca676ef4f8b6cd2c6801fc648cd2cb4
davenkin <davenkin@126.com>
2019-05-14 01:23:58 +0800

    little todo

--- src/main/java/com/ecommerce/order/order/representation/OrderSummaryRepresentation.java
+++ src/main/java/com/ecommerce/order/order/representation/OrderSummaryRepresentation.java
@@ -1,27 +1,29 @@
 package com.ecommerce.order.order.representation;
 
 import com.ecommerce.order.common.ddd.Representation;
+import com.ecommerce.order.common.utils.Address;
 import com.ecommerce.order.order.model.OrderStatus;
 
 import java.math.BigDecimal;
 import java.time.Instant;
-import java.util.List;
 
-// TODO: 2019-05-14 use rabbitmq and cqrs 
+// TODO: 2019-05-14 use rabbitmq and cqrs,order created ; order address changed; order product count changed ; order paid
 public class OrderSummaryRepresentation implements Representation {
     private final String id;
     private final BigDecimal totalPrice;
     private final OrderStatus status;
     private final Instant createdAt;
+    private final Address address;
 
     public OrderSummaryRepresentation(String id,
-                                      List<OrderItemRepresentation> items,
                                       BigDecimal totalPrice,
                                       OrderStatus status,
-                                      Instant createdAt) {
+                                      Instant createdAt,
+                                      Address address) {
         this.id = id;
         this.totalPrice = totalPrice;
         this.status = status;
         this.createdAt = createdAt;
+        this.address = address;
     }
 }
