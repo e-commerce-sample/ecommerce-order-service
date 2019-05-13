02e04a7edcf2b0382f6bcc41adb10e39cfbad570
davenkin <davenkin@126.com>
2019-05-14 01:17:09 +0800

    added task scheduler for shedlock to find

--- src/main/java/com/ecommerce/order/common/distributedlock/DistributedLockConfiguration.java
+++ src/main/java/com/ecommerce/order/common/distributedlock/DistributedLockConfiguration.java
@@ -5,12 +5,21 @@ import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 import javax.sql.DataSource;
 
 @Configuration
 @EnableSchedulerLock(defaultLockAtMostFor = "PT30S")
 public class DistributedLockConfiguration {
+
+    @Bean
+    public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(10);
+        threadPoolTaskScheduler.setThreadNamePrefix("ThreadPoolTaskScheduler-");
+        return threadPoolTaskScheduler;
+    }
 
     @Bean
     public LockProvider lockProvider(DataSource dataSource) {
