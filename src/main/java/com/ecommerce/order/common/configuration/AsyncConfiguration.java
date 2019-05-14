e719d60295194b119d5121321dddc8ac10468d9e
davenkin <davenkin@126.com>
2019-05-14 20:46:45 +0800

    refactored task and scheduling to use custom ones

--- src/main/java/com/ecommerce/order/common/configuration/AsyncConfiguration.java
+++ src/main/java/com/ecommerce/order/common/configuration/AsyncConfiguration.java
@@ -2,29 +2,30 @@ package com.ecommerce.order.common.configuration;
 
 import com.ecommerce.order.common.logging.LogbackMdcTaskDecorator;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
-@EnableScheduling
 public class AsyncConfiguration implements AsyncConfigurer {
-    @Override
-    public Executor getAsyncExecutor() {
+
+    @Bean(destroyMethod = "shutdown")
+    @Primary
+    public TaskExecutor taskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(2);
         executor.setMaxPoolSize(5);
         executor.setQueueCapacity(10);
         executor.setTaskDecorator(new LogbackMdcTaskDecorator());
-        executor.setThreadNamePrefix("MryExecutor-");
         executor.initialize();
         return executor;
     }
+
 
     @Override
     public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
---
+++ src/main/java/com/ecommerce/order/common/configuration/SchedulingConfiguration.java
@@ -0,0 +1,19 @@
+package com.ecommerce.order.common.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfiguration {
+
+    @Bean
+    public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(10);
+        return threadPoolTaskScheduler;
+    }
+
+}
