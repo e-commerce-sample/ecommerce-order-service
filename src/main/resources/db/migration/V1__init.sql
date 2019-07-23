55ecfc397c285245cf99b87ddde05756395266f2
davenkin <davenkin@126.com>
2019-07-23 14:31:56 +0800

    use common db migration

--- src/main/resources/db/migration/V1__init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE shedlock
-(
-  name       VARCHAR(64)  NOT NULL,
-  lock_until DATETIME     NULL,
-  locked_at  DATETIME     NULL,
-  locked_by  VARCHAR(255) NOT NULL,
-  PRIMARY KEY (name)
-);
