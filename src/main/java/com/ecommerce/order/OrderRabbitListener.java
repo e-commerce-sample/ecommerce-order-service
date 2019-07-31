beafdc4d57aca0e4b28cbe7a33b817cdeb8f4f56
davenkin <davenkin@126.com>
2019-07-31 11:03:47 +0800

    pass whole event to event handler

--- src/main/java/com/ecommerce/order/OrderRabbitListener.java
+++ src/main/java/com/ecommerce/order/OrderRabbitListener.java
@@ -2,24 +2,22 @@ package com.ecommerce.order;
 
 import com.ecommerce.common.event.consume.EcommerceRabbitListener;
 import com.ecommerce.common.event.order.OrderEvent;
-import com.ecommerce.order.order.representation.OrderRepresentationService;
+import com.ecommerce.order.order.OrderEventHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.stereotype.Component;
-
-import static com.ecommerce.order.order.model.OrderId.of;
 
 @Component
 @EcommerceRabbitListener
 public class OrderRabbitListener {
-    private final OrderRepresentationService orderRepresentationService;
+    private OrderEventHandler eventHandler;
 
-    public OrderRabbitListener(OrderRepresentationService orderRepresentationService) {
-        this.orderRepresentationService = orderRepresentationService;
+    public OrderRabbitListener(OrderEventHandler eventHandler) {
+        this.eventHandler = eventHandler;
     }
 
     @RabbitHandler
     public void on(OrderEvent event) {
-        orderRepresentationService.cqrsSync(of(event.getOrderId()));
+        eventHandler.cqrsSync(event);
     }
 
 }
---
+++ src/main/java/com/ecommerce/order/order/OrderEventHandler.java
@@ -0,0 +1,20 @@
+package com.ecommerce.order.order;
+
+import com.ecommerce.common.event.order.OrderEvent;
+import com.ecommerce.order.order.representation.OrderRepresentationService;
+import org.springframework.stereotype.Component;
+
+import static com.ecommerce.order.order.model.OrderId.of;
+
+@Component
+public class OrderEventHandler {
+    private final OrderRepresentationService orderRepresentationService;
+
+    public OrderEventHandler(OrderRepresentationService orderRepresentationService) {
+        this.orderRepresentationService = orderRepresentationService;
+    }
+
+    public void cqrsSync(OrderEvent event) {
+        orderRepresentationService.cqrsSync(of(event.getOrderId()));
+    }
+}
