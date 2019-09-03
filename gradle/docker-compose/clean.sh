ad646c4ab0eabec2ca9d03347ac90203e59293c3
davenkin <davenkin@126.com>
2019-09-03 23:59:25 +0800

    added gradle

--- gradle/docker-compose/clean.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-echo "Clean local docker MySQL..."
-
-mysql --protocol=tcp -P 13306 -u root -proot -e "DROP DATABASE IF EXISTS ecommerce_order_mysql;CREATE DATABASE IF NOT EXISTS ecommerce_order_mysql DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci;"
--- gradle/docker-compose/login.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-mysql --protocol=tcp -P 13306 -u root -proot ecommerce_order_mysql
